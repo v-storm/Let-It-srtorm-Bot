8e06f235beaf8e96622c6c1fb7786270b0828e94
v-storm <stormy1861@gmail.com>
2025-12-19 11:07:37 +0000

    fix: correct entrypoint import

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 // index.js — точка входа LET IT STORM (ESM)
 
-import 'dotenv/config';
-
-console.log('ENV loaded. DATABASE_URL:', process.env.DATABASE_URL ? 'OK' : 'MISSING');
-
-import './bot/index.js';
+import "./:bot/index.js";
